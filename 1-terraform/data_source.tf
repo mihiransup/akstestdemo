b34dc41b19b00a58a1559fbfeb44046398404f47
mihiransup <vijaymihiran.n@hashedin.com>
2019-12-13 09:19:20 +0530

    test

--- 1-terraform/data_source.tf
+++ 1-terraform/data_source.tf
@@ -1,6 +1,6 @@
 data "azurerm_key_vault" "terraform_vault" {
-  name                = var.keyvault_name
-  resource_group_name = var.keyvault_rg
+  name                = keyvaulttestingdemo
+  resource_group_name = AksTerraform-RG
 }
 
 data "azurerm_key_vault_secret" "ssh_public_key" {
